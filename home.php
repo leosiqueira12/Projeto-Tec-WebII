d8a76ffc8e769043d7f1ce4292a08ad581222f58
HidekiHiguchi <hideki-higuchi@outlook.com>
2015-03-11 14:59:42 -0300

    What do you think?

--- home.php
+++ home.php
@@ -1,21 +1,82 @@
 <!DOCTYPE html>
 <html>
     <head>
-	<title>Working</title>
-	<link rel="stylesheet" href="style.css" type="text/css">
-    <head>
+        <meta charset="utf-8">
+        <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" media="handheld" href="smartphone.css" type="text/css">
+        <title>OpenSource OS</title>
+    </head>
+    
     <body>
+        
         <header>
-            <nav>
-                <ul class="menu">
-                    <li> <a href="cadastro.php"> Cadastre-se</a></li>
-                    <li> <a href="noticias.php">Noticias </a></li>
-                   
-                </ul>
-            </nav>
 
         </header>
+        <div id="content">
+        
+        <br>
+                <nav>
+  <div class="navicon">
+    <div></div>
+  </div>
+  
+  <a>Home</a>
+  <a>Categories</a>
+  <a>Recommended</a>
+  <a>Blog</a>
+  <a>Contact</a>
+  
+</nav>
+      <div id="article">      
+        <article>
+            <h2>Testing</h2>
+            <p>When working on Lato, Łukasz tried to carefully balance some potentially conflicting priorities. He wanted to create a typeface that would seem quite “transparent” when used in body text but would display some original.</p>
+        </article>
+        
+        <br>
+        
+        <article>
+        
+        </article>
+        
+        <br>
+        
+        <article>
+        
+        </article>
+        
+        <br>
+        
+        <article>
+        
+        </article>
+        
+        <br>
+        
+        <article>
+        
+        </article>
 
+        <br>
+        
+        <article>
+        
+        </article>
+       </div> 
+        <br>
+        
+        </div>
+        
+        <br> <br> <br> <br> <br>
+
+
+        <footer>
+            <img id="twitter" alt="Twitter" src="imagem/twitter.png">
+            <img id="facebook" alt="Facebook" src="imagem/hexagon-facebook_1x.png">
+        </footer>
+        
     </body>
-    <html>
+
+</html>
 
