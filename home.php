75ae50b33d73968b05a09a1c1b6d058684d24875
Everton Davi <evertondavi1990@hotmail.com>
2015-03-11 13:21:08 -0300

    Criando o menu de navegacão.

--- home.php
+++ home.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
-	<html>
-		<head>
-			
-		<head>
-		<body>
-			<header>
-			
-			</header>
-		</body>
-	<html>
+<html>
+    <head>
+
+    <head>
+    <body>
+        <header>
+            <nav>
+                <ul class="menu">
+                    <li> <a href="cadastro.php"> Cadastre-se</a></li>
+                    <li> <a href="noticias.php">Noticias </a></li>
+                   
+                </ul>
+            </nav>
+
+        </header>
+
+    </body>
+    <html>
 
