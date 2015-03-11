775787fd8f6d9a87566f0f4bd370526dccba8f4b
Everton Davi <evertondavi1990@hotmail.com>
2015-03-11 13:37:12 -0300

    Criando a pagina formulario.

---
+++ cadastro.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    <head>
+    <body>
+
+        <form name="form1" method="post" action="enviar.php">
+            <p>Nome: 
+                <input name="nome" type="text" id="nome">
+            </p>
+            <p>Email: 
+                <input name="email" type="text" id="email">
+            </p>
+            <p>Assunto: 
+                <input name="assunto" type="text" id="assunto">
+            </p>
+            <p>Mensagem:<br>
+                <textarea name="mensagem" id="mensagem"></textarea>
+            </p>
+            <p>
+                <input type="submit" name="Submit" value="Enviar">
+            </p>
+        </form>
+    </body>
+    <html>
+
