645019ec5a42b15a73a4c4808721257189f0289f
41435842 <41435842@090027-P33S13.macklabs.br>
2015-03-18 09:33:58 -0300

    Início do Formulário do Psicólogo!

---
+++ cadastro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+        <title>Cadastro de Psicólogos</title>
+        </head>
+        <body>
+   
+<h1> Cadastro de Psicólogos </h1> 
+
+ <form name="cadastro" method="post" action="cadastro.php">
+ <h3>Nome:</h3><input type="text" name="Nome"/>
+ <h3>Sobrenome:</h3><input type="text" name="Sobrenome"/>
+ <h3>Senha:</h3><input type="password" name="Senha" size="8" maxlength="8"/>
+ <h3>E-mail:</h3><input type="email" name="E-mail"/>
+ <h3>Sexo:</h3> <input type="radio" name="sexo" value="Masculino"/>Masculino<input type ="radio" name="sexo" value="Feminino"/>Feminino
+ <br></br>
+ <h3>Ano da Faculdade:</h3><input type="radio" name="Ano da Faculdade" value="1º Ano"/>1º Ano<input type="radio" name="Ano da Faculdade" value="2º Ano"/>2º Ano
+ <input type="radio" name="Ano da Faculdade" value="3º Ano"/>3º Ano<input type="radio" name="Ano da Faculdade" value="4º Ano"/>4º Ano
+ <input type="radio" name="Ano da Faculdade" value="5º Ano"/>5º Ano
+ <br></br>
+ <input type="submit" name="registrar" value="Registrar"/> 
+
+ </body>
+ </html>
